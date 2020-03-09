b649147374ff3506e185626af6d1f8f15e7be589
bossemel <18574762+bossemel@users.noreply.github.com>
2020-03-09 17:19:55 +0000

    combine C1, D4, and test and training for both

--- data_loader.py
+++ data_loader.py
@@ -3,10 +3,20 @@ from src.pyvov import ChipsIndex
 ci = ChipsIndex()
 
 all_experiments = ci.experiments()
-print(all_experiments)
-EXP_NAME = 'A1'
+EXP_NAMES = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'C1', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'E5']
+num_img = 0
+training_split = []
+testing_split = []
+all_labels = []
 
-training_split = ci.training_split_for(EXP_NAME)
-testing_split = ci.testing_split_for(EXP_NAME)
-all = ci.all_for_exp(EXP_NAME)
-labels = ci.labels_for(EXP_NAME)
+for EXP_NAME in ['C1', 'D4']:
+    training_split.extend(ci.training_split_for(EXP_NAME))
+    testing_split.extend(ci.testing_split_for(EXP_NAME))
+    labels = ci.labels_for(EXP_NAME)
+    label_list = list(labels['trn'])
+    label_list.extend(list(labels['tst']))
+    all_labels.extend(label_list)
+
+training_split.extend(testing_split)
+full_dataset = training_split
+print(len(full_dataset), len(all_labels))
