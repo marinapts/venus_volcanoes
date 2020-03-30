d3a2d1a6ed62ddeba4da6d097d7f6da8364c3c03
bossemel <18574762+bossemel@users.noreply.github.com>
2020-03-30 16:22:34 +0100

    small change

--- umap_projection.py
+++ umap_projection.py
@@ -3,21 +3,13 @@ from pca_experiments import load_data, normalize_ds
 import matplotlib.pyplot as plt
 import numpy as np
 
-if __name__ == '__main__':
+colordict = {0: 'navajowhite', 1: 'midnightblue', 2: 'royalblue' , 3: 'cornflowerblue', 4: 'lightskyblue'}
 
-    seed = 8
-    full_dataset, labels = load_data()
-    full_dataset, labels = normalize_ds(full_dataset, labels)
-    smaller_dataset = full_dataset[0:5000]
-    smaller_labels = labels[0:5000]
-    full_dataset = smaller_dataset
-    labels = smaller_labels
-
+def umap_plt(full_dataset, labels):
     reducer = umap.UMAP()
     embedding = reducer.fit_transform(full_dataset)
     print(embedding.shape)
 
-    colordict = {0: 'navajowhite', 1: 'midnightblue', 2: 'royalblue' , 3: 'cornflowerblue', 4: 'lightskyblue'}
 
     # Plot first 2 components
     fig, ax = plt.subplots(1)
@@ -30,8 +22,12 @@ if __name__ == '__main__':
     ax.set_ylabel('Component 2')
     fig.savefig('figures/umap.pdf')
 
+if __name__ == '__main__':
 
-    #plt.scatter(embedding[:, 0], embedding[:, 1]) #, c=[sns.color_palette()[x] for x in iris.target])
-    #plt.gca().set_aspect('equal', 'datalim')
-    #plt.title('UMAP projection of the Iris dataset', fontsize=24)
-    plt.show()
+    seed = 8
+    full_dataset, labels = load_data()
+    full_dataset, labels = normalize_ds(full_dataset, labels)
+    smaller_dataset = full_dataset[0:5000]
+    smaller_labels = labels[0:5000]
+
+    umap_plt(smaller_dataset, smaller_labels)
